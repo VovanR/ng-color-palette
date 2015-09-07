cf300df208b0be766c5f19e8125564ca7bf10f5b
Vladimir Rodkin <mail@vovanr.com>
2015-09-07 21:26:35 +0300

    Init

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ngColorPaletteApp
+ * @description
+ * # ngColorPaletteApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ngColorPaletteApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ngColorPaletteApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ngColorPaletteApp
+ */
+angular.module('ngColorPaletteApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
